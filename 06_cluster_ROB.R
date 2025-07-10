9e494363d2703ceb219da81fe4435afc3c024848
FBartos <f.bartos96@gmail.com>
2025-07-10 06:02:14 -0500

    add RoB processing

---
+++ 06_cluster_ROB.R
@@ -0,0 +1,134 @@
+outdir <- 'out.04.rob.scraper'
+df <- readr::read_tsv( paste0( outdir, '/scraped_rob.tsv' ), quote = "needed" )
+
+head(df)
+length(unique(colnames(df)))
+
+# transform wide to long (this actually takes very long, use the already processed file)
+out <- list()
+tempColnames <- colnames(df)[3:ncol(df)]
+for(i in 1:nrow(df)){
+
+  tempVals <- df[i, 3:ncol(df)]
+  if (all(is.na(tempVals)))
+    next
+
+  out[[i]] <- data.frame(
+    infile       = df$infile[i],
+    study_name   = df$study_name[i],
+    column_name  = tempColnames[!is.na(tempVals)],
+    column_value = tempVals[!is.na(tempVals)]
+  )
+}
+cdf <- do.call(rbind, out)
+
+# remove all rows not containing: "Unclear risk" / "Low risk" / "High risk"
+cdf <- cdf[cdf$column_value %in% c("Unclear risk", "Low risk", "High risk"), ]
+
+write.csv(cdf, paste0(outdir, '/scraped_rob_long.csv'), row.names = FALSE)
+cdf <- read.csv(paste0(outdir, '/scraped_rob_long.csv'), stringsAsFactors = FALSE)
+
+### Risk of Bias categories:
+# Random sequence generation (Selection bias)
+# Allocation concealment (Selection bias)
+# Blinding of participants and personnel (Performance bias)
+# Blinding of outcome assessment (Detection bias)
+# Incomplete outcome data (Attrition bias)
+# Selective outcome reporting (Reporting bias)
+# Other bias
+
+cdf$random_sequence_generation <-
+  grepl("Random_sequence_generation", cdf$column_name, ignore.case = TRUE) |
+  grepl("Randomisation_method_reported", cdf$column_name, ignore.case = TRUE) |
+  grepl("Sequence_generation", cdf$column_name, ignore.case = TRUE) |
+  grepl("Adequate_sequence_generation", cdf$column_name, ignore.case = TRUE)
+
+
+cdf$allocation_concealment <-
+  grepl("Allocation_concealment", cdf$column_name, ignore.case = TRUE) |
+  grepl("Knowledge_of_allocated_intervention", cdf$column_name, ignore.case = TRUE) |
+  grepl("Knowledge_of_the_allocation", cdf$column_name, ignore.case = TRUE)
+
+cdf$blinding_of_participants_and_personnel <-
+  grepl("Blinding_of_participants", cdf$column_name, ignore.case = TRUE) |
+  grepl("Blinding_of_participants", cdf$column_name, ignore.case = TRUE) |
+  grepl("Masking_of_participants", cdf$column_name, ignore.case = TRUE) |
+  grepl("Blinding_of_care_provider", cdf$column_name, ignore.case = TRUE) |
+  grepl("Blinding_of_caregiver", cdf$column_name, ignore.case = TRUE) |
+  grepl("Blinding_of_personnel", cdf$column_name, ignore.case = TRUE) |
+  grepl("Blinding_of_participant", cdf$column_name, ignore.case = TRUE) |
+  grepl("Blinding_of_provider", cdf$column_name, ignore.case = TRUE) |
+  grepl("Blinding_of_doctors_personnel", cdf$column_name, ignore.case = TRUE)
+
+
+cdf$blinding_of_outcome_assessment <-
+  grepl("Blinding_of_outcome_assessment", cdf$column_name, ignore.case = TRUE) |
+  grepl("Blinding_of_outcome_assessors", cdf$column_name, ignore.case = TRUE) |
+  grepl("Masking_of_outcome_assessment", cdf$column_name, ignore.case = TRUE) |
+  grepl("Masking_of_outcome_assessors", cdf$column_name, ignore.case = TRUE) |
+  grepl("Blinding_of_assessment", cdf$column_name, ignore.case = TRUE) |
+  grepl("Blinding_of_the_Outcome_assessor", cdf$column_name, ignore.case = TRUE) |
+  grepl("Blinding_Outcome_assessor", cdf$column_name, ignore.case = TRUE) |
+  grepl("Blinding_of_radiological_outcomes", cdf$column_name, ignore.case = TRUE) |
+  grepl("Blinding_of_clinical_outcomes", cdf$column_name, ignore.case = TRUE)
+# Timing_of_outcome_assessment_similar
+# Timing_outcome_assessments
+
+cdf$incomplete_outcome_data <-
+  grepl("Incomplete_outcome_data", cdf$column_name, ignore.case = TRUE) |
+  grepl("Loss_of_clusters", cdf$column_name, ignore.case = TRUE) |
+  grepl("Incomplete_follow_up", cdf$column_name, ignore.case = TRUE) |
+  grepl("Intention_to_treat_analysis", cdf$column_name, ignore.case = TRUE) |
+  grepl("Participants_analysed_in_group_allocated", cdf$column_name, ignore.case = TRUE) |
+  grepl("ITT_analysis", cdf$column_name, ignore.case = TRUE) |
+  grepl("Intention_to_treat", cdf$column_name, ignore.case = TRUE) |
+  grepl("Incomplete_outcome_assessment", cdf$column_name, ignore.case = TRUE)
+
+cdf$selective_outcome_reporting <-
+  grepl("Selective_outcome_reporting", cdf$column_name, ignore.case = TRUE) |
+  grepl("Bias_in_selection_of_the_reported", cdf$column_name, ignore.case = TRUE) |
+  grepl("Selective_reporting", cdf$column_name, ignore.case = TRUE)
+# Selection_of_the_non_exposed_cohort
+
+cdf$other_bias <- grepl("Other_bias", cdf$column_name, ignore.case = TRUE)
+
+
+table(cdf$random_sequence_generation)
+table(cdf$allocation_concealment)
+table(cdf$blinding_of_participants_and_personnel)
+table(cdf$blinding_of_outcome_assessment)
+table(cdf$incomplete_outcome_data)
+table(cdf$selective_outcome_reporting)
+table(cdf$other_bias)
+
+# check which columns are missing
+cdf$unknown <- apply(cdf[,c(
+  "random_sequence_generation",
+  "allocation_concealment",
+  "blinding_of_participants_and_personnel",
+  "blinding_of_outcome_assessment",
+  "incomplete_outcome_data",
+  "selective_outcome_reporting",
+  "other_bias"
+)], 1, function(x) all(!x))
+round(table(cdf$unknown) / length(cdf$unknown), 3)
+
+# sort the missing columns by frequency -- update the name dispatching
+sort(table(cdf$column_name[cdf$unknown]))
+
+# this one could be both the "blinding_of_outcome_assessment" or "blinding_of_participants_and_personnel"
+# (especially since there is "performance_bias_and_detection_bias")
+# Blinding_performance_bias_and_detection_bias_All_outcomes
+
+# verify that only one category was assigned
+cdf$number_cat <- apply(cdf[,c(
+  "random_sequence_generation",
+  "allocation_concealment",
+  "blinding_of_participants_and_personnel",
+  "blinding_of_outcome_assessment",
+  "incomplete_outcome_data",
+  "selective_outcome_reporting",
+  "other_bias"
+)], 1, function(x) sum(x))
+table(cdf$number_cat) # only 0/1 parallel categories assigned = correct
+write.csv(cdf, paste0(outdir, '/scraped_rob_long_processed.csv'), row.names = FALSE)
